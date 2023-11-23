481e25043d8fba4ca4eabef1abcf8f7e49c4cac2
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2023-11-23 12:04:17 -0300

    first commit

---
+++ src/main/java/br/com/alura/TabelaFIPEAPI/TabelaFipeapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.TabelaFIPEAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFipeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFipeapiApplication.class, args);
+	}
+
+}
